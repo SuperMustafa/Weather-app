9637d02915981226287761438cbbb02e0c78325f
Mustafa Hamad <mustafahamad116aa@gmail.com>
2024-08-31 03:53:22 +0300

    Update contactError.js

--- js/contactError.js
+++ js/contactError.js
@@ -5,8 +5,8 @@ var error=document.querySelectorAll(".error");
 
 for(var i=0;i<error.length;i++){
     error[i].addEventListener("click",function(){
-        if(location.href=="file:///E:/Frontend/Deliverd%20Assignments/weather%20App/contact.html"){
-            open("file:///E:/Frontend/Deliverd%20Assignments/weather%20App/error.html")
+        if(location.href=="contact.html"){
+            open("error.html")
         }
     })
 }
