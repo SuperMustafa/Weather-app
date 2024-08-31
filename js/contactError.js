22698073a03c8da6f52896861d29f91ece43f7b6
Mustafa Hamad <mustafahamad116aa@gmail.com>
2024-08-31 04:31:16 +0300

    Update contactError.js

--- js/contactError.js
+++ js/contactError.js
@@ -5,7 +5,7 @@ var error=document.querySelectorAll(".error");
 
 for(var i=0;i<error.length;i++){
     error[i].addEventListener("click",function(){
-        if(location.href=="contact.html"){
+        if(location.href=="contact.html"||location.href=="index.html"){
             open("error.html")
         }
     })
