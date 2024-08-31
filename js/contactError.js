87ae2614c8fbc34d570ca15f0ba3cce341bf162c
Mustafa Hamad <mustafahamad116aa@gmail.com>
2024-08-31 03:41:32 +0300

    second commit

--- js/contactError.js
+++ js/contactError.js
@@ -5,8 +5,8 @@ var error=document.querySelectorAll(".error");
 
 for(var i=0;i<error.length;i++){
     error[i].addEventListener("click",function(){
-        if(location.href=="file:///E:/Frontend/Deliverd%20Assignments/js-assignment-5/contact.html"){
-            open("file:///E:/Frontend/Deliverd%20Assignments/js-assignment-5/error.html")
+        if(location.href=="file:///E:/Frontend/Deliverd%20Assignments/weather%20App/contact.html"){
+            open("file:///E:/Frontend/Deliverd%20Assignments/weather%20App/error.html")
         }
     })
 }
